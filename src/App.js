645d09a4edc232b2abd411678fdb2c3b3fc23608
yousofkortam <yousofkortam@outlook.com>
2023-12-20 21:01:49 +0200

    Add about & contact routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProductDetails from './components/ProductDetails';
 import Products_List from './components/Products_List';
+import About from './pages/about';
+import Contact from './pages/contact';
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/product/:productId" element={<ProductDetails />} />
           <Route path="/" element={<Products_List />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </div>
