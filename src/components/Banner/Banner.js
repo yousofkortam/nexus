d9c398df1291fa8b84fec844f29acf8e1567d0d2
yousofkortam <yousofkortam@outlook.com>
2023-12-24 19:15:48 +0200

    Add Banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -16,24 +16,30 @@ function Banner() {
     slidesToScroll: 1,
   };
 
+  const bannerStyle = {
+    width: '100%',
+    maxHeight: '65vh',
+    objectFit: 'cover', // This property ensures the image covers the entire container
+  };
+
   return (
     <Slider {...settings}>
       <div className="banner-container">
-        <img src={bannerImage1} alt="Nexus E-Commerce" className="img-fluid" />
+        <img src={bannerImage1} alt="Nexus E-Commerce" className="img-fluid" style={bannerStyle} />
         <div className="banner-text">
-          <h1 className="display-4">Welcome to Nexus</h1>
+          <h1 className="display-4 mt-3">Welcome to Nexus</h1>
           <p className="lead">Your one-stop destination for all your shopping needs.</p>
         </div>
       </div>
       <div className="banner-container">
-        <img src={bannerImage2} alt="Nexus E-Commerce" className="img-fluid" style={{ maxHeight: '500px' }} />
+        <img src={bannerImage2} alt="Nexus E-Commerce" className="img-fluid" style={bannerStyle} />
         <div className="banner-text">
           <h1 className="display-4">Discover the Latest Trends</h1>
           <p className="lead">Explore a wide range of products and stay in style.</p>
         </div>
       </div>
       <div className="banner-container">
-        <img src={bannerImage3} alt="Nexus E-Commerce" className="img-fluid" style={{ maxHeight: '500px' }} />
+        <img src={bannerImage3} alt="Nexus E-Commerce" className="img-fluid" style={bannerStyle} />
         <div className="banner-text">
           <h1 className="display-4">Shop with Confidence</h1>
           <p className="lead">Secure and seamless shopping experience for our customers.</p>
